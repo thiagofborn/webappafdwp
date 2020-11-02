8553990e87cce4b29b6de34df03568286a317c45
Thiago Born <thborn@microsoft.com>
2020-11-02 16:54:38 +0000

    cleaning

--- wp-config.php
+++ wp-config.php
@@ -117,11 +117,7 @@ define('WP_DEBUG', true);
 
 /* That's all, stop editing! Happy blogging. */
 define('MULTISITE', true);
-define('SUBDOMAIN_INSTALL', true);
-define('DOMAIN_CURRENT_SITE', 'www.wastelandweb.com');
-define('PATH_CURRENT_SITE', '/');
-define('SITE_ID_CURRENT_SITE', 1);
-define('BLOG_ID_CURRENT_SITE', 1);
+
 
 //Relative URLs for swapping across app service deployment slots 
 
